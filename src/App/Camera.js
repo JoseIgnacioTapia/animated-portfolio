b4dd3e61256b0268f8336c54abe874804ed3aa87
JoseIgnacioTapia <josignaciot@gmail.com>
2024-08-27 16:38:20 -0300

    Resizing and Zustand

--- src/App/Camera.js
+++ src/App/Camera.js
@@ -1,19 +1,26 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import App from './App';
+import { sizesStore } from './Utils/Store';
 
 export default class Camera {
   constructor() {
     this.app = new App();
     this.canvas = this.app.canvas;
+
+    this.sizesStore = sizesStore;
+
+    this.sizes = sizesStore.getState();
+
     this.setInstance();
     this.setControls();
+    this.setResizeLister();
   }
 
   setInstance() {
     this.instance = new THREE.PerspectiveCamera(
       35,
-      window.innerWidth / window.innerHeight,
+      this.sizes.width / this.sizes.height,
       0.1,
       200
     );
@@ -23,6 +30,13 @@ export default class Camera {
   setControls() {
     this.controls = new OrbitControls(this.instance, this.canvas);
     this.controls.enableDamping = true;
+  }
+
+  setResizeLister() {
+    this.sizesStore.subscribe((sizes) => {
+      this.instance.aspect = sizes.width / sizes.height;
+      this.instance.updateProjectionMatrix();
+    });
   }
 
   loop() {
