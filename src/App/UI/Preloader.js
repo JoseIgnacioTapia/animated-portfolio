be4a7c4d76a756535f4dbad26c94688fe9629e19
JoseIgnacioTapia <josignaciot@gmail.com>
2024-08-28 10:10:56 -0300

    Preloader

---
+++ src/App/UI/Preloader.js
@@ -0,0 +1,18 @@
+import assetStore from '../Utils/AssetStore';
+
+export default class Preloader {
+  constructor() {
+    this.assetStore = assetStore;
+
+    this.assetStore.subscribe((state) => {
+      //   console.log(state.loadAssets);
+
+      this.numberOfLoadedAssets = Object.keys(state.loadedAssets).length;
+      this.numberOfAssetsToLoad = state.assetsToLoad.length;
+
+      this.progress = this.numberOfLoadedAssets / this.numberOfAssetsToLoad;
+
+      console.log(this.progress);
+    });
+  }
+}
